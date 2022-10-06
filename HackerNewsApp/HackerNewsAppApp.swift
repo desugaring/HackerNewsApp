8cf5317960797f4146b9bb83f9cf7a3c52a682dd
alex.semenikhine <alexsem@gmail.com>
2022-10-06 15:27:10 -0400

    Initial Commit

---
+++ HackerNewsApp/HackerNewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsAppApp.swift
+//  HackerNewsApp
+//
+//  Created by Alex Semenikhine on 2022-10-06.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
